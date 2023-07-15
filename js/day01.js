f3b9ba82919048ade09c5b96ec95b2c0fcb62bfa
shivaArena <shivaprasad.arenaameerpet@gmail.com>
2023-07-15 09:07:39 +0530

    9 to 10 Web Design

---
+++ js/day01.js
@@ -0,0 +1,124 @@
+// alert("welcome to javascript")
+/*
+// variables
+var name
+var car
+var name99 
+var _name
+var $name
+var firstName 
+var Name 
+// invalid name for variables
+//var first name invalid
+// var first-name invalid
+// var 99 invalid
+name = "avgc"
+console.log(name)
+car = "tata"
+console.log(car)
+var computer = "dell"
+console.log(computer)
+
+// var , let , const
+
+let city = 'hyderabad'
+console.log(city)
+
+const mobile = "oppo"
+console.log(mobile)
+
+// var is global variable
+// let block scope variable
+// const data can not change
+
+var bag = "milton"
+console.log(bag)
+{
+    var brand = "mike"
+    console.log(bag)   
+}
+
+console.log(brand)
+let speaker = "F&D"
+speaker = "sony"
+{
+    let remote = "sony"
+    console.log(speaker)
+    console.log(remote)
+}
+// console.log(remote)
+var bag = "samsung"
+// let speaker = "obage"
+const keys = "bike"
+console.log(keys)
+keys ="car"
+*/
+
+// Data types in javascript
+
+// Primitive datatypes and object
+// String
+// Number
+// null
+// Boolean
+// undefined
+// BigInt
+// Symbol
+
+// object date type
+// object
+
+// string datatype 
+/*
+let speaker = "F&D"
+let keys = 'bike'
+let greet = 'welcome Mr. "shiva" ! '
+let msg = "javascript 'easy to learn' "
+let longPhr = `you ca use back ticks to create string`
+let rule = "welcome to \"world\" "
+
+console.log(speaker, keys, greet)
+console.log(msg)
+console.log(longPhr)
+console.log(rule)
+
+// Number datatype
+let a = 10
+let b = 2.5
+let c = -9
+
+console.log(a, b, c)
+console.log(typeof speaker, typeof a)
+
+// BigInt
+let bigNum = 50000000000098870000654560n
+console.log(typeof bigNum)
+let lNum = BigInt(50)
+console.log(lNum)
+
+// Boolean  True or False
+let human = true
+console.log(typeof human)
+
+// undefined
+let watch 
+console.log(typeof watch)
+
+// null  means empty
+let ring = null
+console.log(typeof null)
+
+// Object
+let speakers = {
+    brand:"F&B",
+    type : 5.1,
+    price: 5000,
+    stock: null
+}
+console.log(speakers)
+console.log(speakers.price)
+console.log(typeof speakers)
+*/
+
+
+
