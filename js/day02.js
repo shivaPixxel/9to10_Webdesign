1ebc7b6c170c4c413cf3fc24551035cee5d81bb3
shivaArena <shivaprasad.arenaameerpet@gmail.com>
2023-07-15 10:04:50 +0530

    day02

---
+++ js/day02.js
@@ -0,0 +1,90 @@
+// Operators in javascript
+
+// Assignment Operators
+// = += -= *= /= %=
+/*
+let x = 10 // 10
+console.log(x)
+x += 5 // 15
+console.log(x)
+x -= 5 // 10
+console.log(x)
+x *= 2 // 20
+console.log(x)
+x /= 5 // 4
+console.log(x)
+x **= 2
+console.log(x)
+x %= 2 // 0
+console.log(x)
+*/
+
+// Arithmetic operators
+// + - * / % ++ --
+
+/*
+let a = 2, b = 3, c = 10
+console.log(a+b) // 5
+console.log(c-b) // 7
+console.log(a*b) // 6
+console.log(c/a) // 5
+console.log(c%a) // 0
+c++
+console.log(c) // 11
+c-- 
+console.log(10)
+console.log(++c) // 11
+console.log(--c) // 10
+c += 1 // c++
+c -= 1 // c--
+*/
+
+// Comparison Operators
+// == === < > >= <= !==
+/*
+let i = 10, foo = "10", bar = 50
+console.log(i<bar)
+console.log(i==foo, typeof i, typeof foo)
+console.log(i===foo)
+console.log(i!=foo)
+console.log(i!==foo)
+
+// Logical Operators
+// && || !
+// let student = false; fee = true; job = false
+// console.log(student === true && fee === true && job == false)
+// console.log(student === true || fee === true || job === true)
+
+console.log(student === !true && fee === !true)
+*/
+/*
+let a = "11+2", b = 2
+let result = a/b
+console.log(result, typeof result) // 102
+console.log(Number.parseInt(a))
+*/
+
+/*
+let age = 12, exam = false
+
+if(age >= 18 || exam == true){
+    console.log("he can ride bike")
+}else{
+    console.log("he is under age")
+}
+*/
+
+/*
+let lang = prompt("enter 1 to 3 number")
+console.log(lang)
+
+if(lang == 1){
+    console.log("your language is english")
+}else if(lang == 2){
+    console.log("your language is hindi")
+}else if(lang == 3){
+    console.log("your language is telugu")
+}else{
+    console.log("enter a number between 1 to 3")
+}
+*/
